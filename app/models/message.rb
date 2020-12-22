66e8608142de1f557de023fbbf9730661ab42b97
kiserukun <73691576+kiserukun@users.noreply.github.com>
2020-12-23 01:20:29 +0900

    写真複数枚

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,11 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
-  has_one_attached :image
+  has_many_attached :images
   
   validates :content, presence: true, unless: :was_attached?
 
   def was_attached?
-    self.image.attached?
+    self.images.attached?
   end
 end
